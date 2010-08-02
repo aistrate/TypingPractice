d397a6a1e9f0dee23d86c2492e58c358cab348d1
Adrian Istrate <adrian.istrate@yahoo.com>
2010-08-02 22:56:55 +0200

    Basic version, with mirroring over the original text.

---
+++ Typist/Typist/Typist.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Typist
+{
+    partial class Typist
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImport = new System.Windows.Forms.Button();
+            this.ofdImport = new System.Windows.Forms.OpenFileDialog();
+            this.pbTyping = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.rtbImportedText = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTyping)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(13, 12);
+            this.btnImport.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(120, 25);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import...";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // ofdImport
+            // 
+            this.ofdImport.DefaultExt = "txt";
+            this.ofdImport.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            // 
+            // pbTyping
+            // 
+            this.pbTyping.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbTyping.BackColor = System.Drawing.Color.White;
+            this.pbTyping.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbTyping.Location = new System.Drawing.Point(13, 52);
+            this.pbTyping.Name = "pbTyping";
+            this.pbTyping.Size = new System.Drawing.Size(621, 224);
+            this.pbTyping.TabIndex = 1;
+            this.pbTyping.TabStop = false;
+            this.pbTyping.Click += new System.EventHandler(this.pbTyping_Click);
+            this.pbTyping.Resize += new System.EventHandler(this.pbTyping_Resize);
+            this.pbTyping.Paint += new System.Windows.Forms.PaintEventHandler(this.pbTyping_Paint);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(514, 12);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(120, 25);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // rtbImportedText
+            // 
+            this.rtbImportedText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbImportedText.BackColor = System.Drawing.Color.White;
+            this.rtbImportedText.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbImportedText.Location = new System.Drawing.Point(12, 282);
+            this.rtbImportedText.Name = "rtbImportedText";
+            this.rtbImportedText.ReadOnly = true;
+            this.rtbImportedText.Size = new System.Drawing.Size(622, 224);
+            this.rtbImportedText.TabIndex = 4;
+            this.rtbImportedText.Text = "";
+            // 
+            // Typist
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 518);
+            this.Controls.Add(this.rtbImportedText);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pbTyping);
+            this.Controls.Add(this.btnImport);
+            this.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MinimumSize = new System.Drawing.Size(300, 500);
+            this.Name = "Typist";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Typist";
+            this.Load += new System.EventHandler(this.Typist_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Typist_KeyPress);
+            this.Move += new System.EventHandler(this.Typist_Move);
+            ((System.ComponentModel.ISupportInitialize)(this.pbTyping)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.OpenFileDialog ofdImport;
+        private System.Windows.Forms.PictureBox pbTyping;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox rtbImportedText;
+    }
+}
+
---
+++ Typist/Typist/Typist.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Typist
+{
+    public partial class Typist : Form
+    {
+        public Typist()
+        {
+            InitializeComponent();
+            TypedText = new StringBuilder();
+        }
+
+        private void Typist_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        protected string ImportedText { get; private set; }
+
+        protected StringBuilder TypedText { get; private set; }
+
+        protected bool PracticeMode { get; private set; }
+
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            if (ofdImport.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(ofdImport.FileName))
+                    ImportedText = sr.ReadToEnd();
+
+                TypedText = new StringBuilder();
+                pbTyping.Refresh();
+
+                rtbImportedText.Text = ImportedText;
+
+                PracticeMode = false;
+                btnStart_Click(btnStart, EventArgs.Empty);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            PracticeMode = !PracticeMode;
+            btnStart.Text = PracticeMode ? "Pause" : "Start";
+            rtbImportedText.Focus();
+        }
+
+        private void pbTyping_Paint(object sender, PaintEventArgs e)
+        {
+            drawText(ImportedText, e.Graphics, e.ClipRectangle, Brushes.Black);
+            drawText(TypedText.ToString(), e.Graphics, e.ClipRectangle, Brushes.Red);
+        }
+
+        private void drawText(string text, Graphics graphics, Rectangle rectangle, Brush brush)
+        {
+            StringFormat stringFormat = new StringFormat(StringFormatFlags.LineLimit)
+            {
+                Trimming = StringTrimming.None,
+                HotkeyPrefix = System.Drawing.Text.HotkeyPrefix.Show
+            };
+
+            graphics.DrawString(text,
+                                new Font("Courier New", 9),
+                                brush,
+                                new Rectangle()
+                                {
+                                    X = 1,
+                                    Y = 2,
+                                    Width = rectangle.Width - 24,
+                                    Height = rectangle.Height - 4
+                                },
+                                stringFormat);
+
+            //stringFormat.SetMeasurableCharacterRanges(new[]
+            //    {
+            //        new CharacterRange(0, 100),
+            //        new CharacterRange(100, 200),
+            //        new CharacterRange(200, 300),
+            //        new CharacterRange(300, 400),
+            //    });
+
+            //Region[] regions = graphics.MeasureCharacterRanges(text,
+            //             new Font("Courier New", 9),
+            //             new Rectangle()
+            //             {
+            //                 X = 1,
+            //                 Y = 2,
+            //                 Width = rectangle.Width - 24,
+            //                 Height = rectangle.Height - 4
+            //             },
+            //             stringFormat);
+
+            //foreach (var region in regions)
+            //{
+            //    RectangleF rectF = region.GetBounds(g);
+            //    graphics.DrawRectangle(Pens.Black, new Rectangle((int)rectF.X, (int)rectF.Y, (int)rectF.Width, (int)rectF.Height));
+            //}
+        }
+
+        private void pbTyping_Resize(object sender, EventArgs e)
+        {
+            pbTyping.Refresh();
+        }
+
+        private void pbTyping_Click(object sender, EventArgs e)
+        {
+            pbTyping.Refresh();
+        }
+
+        private void Typist_Move(object sender, EventArgs e)
+        {
+            pbTyping.Refresh();
+        }
+
+        private void Typist_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (PracticeMode)
+            {
+                if (e.KeyChar == '\b')
+                {
+                    if (TypedText.Length > 0)
+                        TypedText.Remove(TypedText.Length - 1, 1);
+                }
+                else
+                {
+                    string str;
+                    switch (e.KeyChar)
+                    {
+                        case '\r':
+                            str = "\n";
+                            break;
+                        default:
+                            str = e.KeyChar.ToString();
+                            break;
+                    }
+
+                    TypedText.Append(str);
+                }
+
+                pbTyping.Refresh();
+            }
+
+            e.Handled = true;
+        }
+    }
+}
