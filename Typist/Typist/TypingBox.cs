e63b59b7a67d0feff9d427ae196eca933ec7373d
Adrian Istrate <adrian.istrate@yahoo.com>
2010-08-11 00:01:42 +0200

    Introduce GraphicsContext class (to pass around Graphics data from the Paint event handler).

--- Typist/Typist/TypingBox.cs
+++ Typist/Typist/TypingBox.cs
@@ -122,61 +122,65 @@ namespace Typist
 
             if (!DesignMode)
             {
-                RectangleF typingArea = calcTypingArea(pe.Graphics);
+                GraphicsContext gc  = getGraphicsContext(pe.Graphics);
 
-                drawImportedText(pe.Graphics, typingArea);
-                drawShadowText(pe.Graphics, typingArea);
-                drawErrorChars(pe.Graphics, typingArea);
-                drawCursor(pe.Graphics, typingArea);
+                drawImportedText(gc);
+                drawShadowText(gc);
+                drawErrorChars(gc);
+                drawCursor(gc);
             }
         }
 
-        private RectangleF calcTypingArea(Graphics g)
+        private GraphicsContext getGraphicsContext(Graphics g)
         {
-            RectangleF sampleCharArea = getCharArea("_", 0, g, g.ClipBounds);
-
-            return new RectangleF()
+            RectangleF typingArea = new RectangleF()
             {
                 X = MarginLeft,
                 Y = MarginTop,
-                Width = g.ClipBounds.Width - MarginLeft - MarginRight - sampleCharArea.Width,
-                Height = g.ClipBounds.Height - MarginTop - MarginBottom,
+                Width = g.ClipBounds.Width - (MarginLeft + MarginRight),
+                Height = g.ClipBounds.Height - (MarginTop + MarginBottom),
+            };
+
+            RectangleF firstCharArea = getCharAreas32("_", new[] { 0 }, g, typingArea).First();
+
+            typingArea.Width -= firstCharArea.Width;
+
+            return new GraphicsContext()
+            {
+                Graphics = g,
+                TypingArea = typingArea,
+                FirstCharArea = firstCharArea,
             };
         }
 
-        private void drawImportedText(Graphics g, RectangleF typingArea)
+        private void drawImportedText(GraphicsContext gc)
         {
-            drawText(ImportedText.ToString(), g, Theme.ImportedTextColor, typingArea);
+            drawText(ImportedText.ToString(), gc, Theme.ImportedTextColor);
         }
 
-        private void drawShadowText(Graphics g, RectangleF typingArea)
+        private void drawShadowText(GraphicsContext gc)
         {
             string shadowText = ImportedText.Substring(0, TypedText.Length);
 
-            int lineJumpIndex = findLineJump(ImportedText.ToString(), TypedText.LastIndex,
-                                             g, typingArea);
+            int lineJumpIndex = findLineJump(ImportedText.ToString(), TypedText.LastIndex, gc);
             if (lineJumpIndex < TypedText.LastIndex)
                 shadowText = insertSpacesAfterJump(ImportedText.ToString(), shadowText,
                                                    lineJumpIndex, TypedText.LastIndex,
-                                                   g, typingArea);
+                                                   gc);
 
-            drawText(shadowText, g, Theme.TypedTextColor, typingArea);
+            drawText(shadowText, gc, Theme.TypedTextColor);
         }
 
-        private int findLineJump(string text, int lastIndex, Graphics g, RectangleF typingArea)
+        private int findLineJump(string text, int lastIndex, GraphicsContext gc)
         {
             RectangleF importedCharArea, shadowCharArea;
             int lineJumpIndex = lastIndex;
 
             while (lineJumpIndex >= 0)
             {
-                importedCharArea = getCharArea(text,
-                                               lineJumpIndex,
-                                               g, typingArea);
+                importedCharArea = getCharArea(text, lineJumpIndex, gc);
 
-                shadowCharArea = getCharArea(text.Substring(0, lineJumpIndex + 1),
-                                             lineJumpIndex,
-                                             g, typingArea);
+                shadowCharArea = getCharArea(text.Substring(0, lineJumpIndex + 1), lineJumpIndex, gc);
 
                 if (shadowCharArea.Y == importedCharArea.Y)
                     return lineJumpIndex;
@@ -187,12 +191,9 @@ namespace Typist
             return lastIndex;
         }
 
-        private string insertSpacesAfterJump(string text, string shadowText, int lineJumpIndex, int lastIndex,
-                                             Graphics g, RectangleF typingArea)
+        private string insertSpacesAfterJump(string text, string shadowText, int lineJumpIndex, int lastIndex, GraphicsContext gc)
         {
-            RectangleF importedCharArea = getCharArea(text,
-                                                      lastIndex,
-                                                      g, typingArea);
+            RectangleF importedCharArea = getCharArea(text, lastIndex, gc);
 
             RectangleF shadowCharArea;
             int spaces = 0;
@@ -202,32 +203,29 @@ namespace Typist
                 shadowText = shadowText.Insert(lineJumpIndex + 1, " ");
                 spaces++;
 
-                shadowCharArea = getCharArea(shadowText,
-                                             lastIndex + spaces,
-                                             g, typingArea);
+                shadowCharArea = getCharArea(shadowText, lastIndex + spaces, gc);
             }
             while (shadowCharArea.Y < importedCharArea.Y);
 
             return shadowText;
         }
 
-        private void drawErrorChars(Graphics g, RectangleF typingArea)
+        private void drawErrorChars(GraphicsContext gc)
         {
             RectangleF[] errorCharAreas = getCharAreas(ImportedText.ToString(),
-                                                       TypedText.ErrorsUncorrected.ToArray(),
-                                                       g, typingArea);
+                                                       TypedText.ErrorsUncorrected.ToArray(), gc);
 
             for (int i = 0; i < TypedText.ErrorsUncorrected.Count; i++)
             {
-                g.FillRectangle(Theme.ErrorBackColor, fracOffsetCharArea(errorCharAreas[i], 0, ErrorBackgroundVOffset));
+                gc.Graphics.FillRectangle(Theme.ErrorBackColor, fracOffsetCharArea(errorCharAreas[i], 0, ErrorBackgroundVOffset));
 
                 drawChar(TypedText[TypedText.ErrorsUncorrected[i]],
-                         g, Theme.ErrorForeColor,
+                         gc, Theme.ErrorForeColor,
                          errorCharAreas[i]);
             }
         }
 
-        private void drawCursor(Graphics g, RectangleF typingArea)
+        private void drawCursor(GraphicsContext gc)
         {
             CancelEventArgs e = new CancelEventArgs(false);
 
@@ -236,14 +234,13 @@ namespace Typist
             if (!e.Cancel)
             {
                 RectangleF cursorArea = getCharArea(ImportedText.ToString(),
-                                                    TypedText.LastIndex + 1,
-                                                    g, typingArea);
+                                                    TypedText.LastIndex + 1, gc);
 
                 cursorArea = fracOffsetCharArea(cursorArea, 0,
                                                 CursorAsVerticalBar ? BarCursorVOffset : CharCursorVOffset);
 
                 if (CursorAsVerticalBar)
-                    g.FillRectangle(Theme.CursorColor, new RectangleF()
+                    gc.Graphics.FillRectangle(Theme.CursorColor, new RectangleF()
                     {
                         X = cursorArea.X - (0.125f * cursorArea.Width) - (0.5f * Theme.BarCursorLineWidth),
                         Y = cursorArea.Y,
@@ -251,7 +248,7 @@ namespace Typist
                         Height = cursorArea.Height,
                     });
                 else
-                    drawChar(CharCursorChar, g, Theme.CursorColor, cursorArea);
+                    drawChar(CharCursorChar, gc, Theme.CursorColor, cursorArea);
             }
         }
 
@@ -273,44 +270,48 @@ namespace Typist
 
         private char pilcrow = '\xB6';
 
-        private void drawChar(char ch, Graphics g, Brush brush, RectangleF charArea)
+        private void drawChar(char ch, GraphicsContext gc, Brush brush, RectangleF charArea)
         {
             if (ch == '\n')
                 ch = pilcrow;
 
-            g.DrawString(ch.ToString(), Theme.Font, brush, charArea, SingleCharStringFormat);
+            gc.Graphics.DrawString(ch.ToString(), Theme.Font, brush, charArea, SingleCharStringFormat);
         }
 
-        private void drawText(string text, Graphics g, Brush brush, RectangleF typingArea)
+        private void drawText(string text, GraphicsContext gc, Brush brush)
         {
             if (VisibleNewlines)
                 text = text.Replace("\n", string.Format("{0}\n", pilcrow));
 
-            g.DrawString(text, Theme.Font, brush, typingArea, TextStringFormat);
+            gc.Graphics.DrawString(text, Theme.Font, brush, gc.TypingArea, TextStringFormat);
         }
 
-        private RectangleF getCharArea(string text, int charIndex, Graphics g, RectangleF typingArea)
+        private RectangleF getCharArea(string text, int charIndex, GraphicsContext gc)
         {
-            return getCharAreas(text, new[] { charIndex }, g, typingArea).First();
+            return getCharAreas(text, new[] { charIndex }, gc).First();
         }
 
-        private RectangleF[] getCharAreas(string text, int[] charIndexes, Graphics g, RectangleF typingArea)
+        private RectangleF[] getCharAreas(string text, int[] charIndexes, GraphicsContext gc)
         {
             charIndexes = charIndexes.Where(i => i < text.Length)
                                      .ToArray();
 
             RectangleF[] charAreas = charIndexes.Split(32)
-                                                .SelectMany(grp => getCharAreas32(text, grp.ToArray(),
-                                                                                  g, typingArea))
+                                                .SelectMany(grp => getCharAreas32(text, grp.ToArray(), gc))
                                                 .ToArray();
 
             for (int i = 0; i < charIndexes.Length; i++)
                 if (charAreas[i].IsEmpty)
                     charAreas[i] = getCharAreas32(text.Insert(charIndexes[i], "-"),
                                                   new[] { charIndexes[i] },
-                                                  g, typingArea)[0];
+                                                  gc)[0];
 
             return charAreas;
+        }
+
+        private RectangleF[] getCharAreas32(string text, int[] charIndexes, GraphicsContext gc)
+        {
+            return getCharAreas32(text, charIndexes, gc.Graphics, gc.TypingArea);
         }
 
         private RectangleF[] getCharAreas32(string text, int[] charIndexes, Graphics g, RectangleF typingArea)
@@ -324,6 +325,15 @@ namespace Typist
 
             return regions.Select(r => r.GetBounds(g))
                           .ToArray();
+        }
+
+        private void drawDebugMessage(string message, GraphicsContext gc)
+        {
+            RectangleF position = new RectangleF(gc.TypingArea.X, gc.TypingArea.Y + gc.TypingArea.Height - 16, 
+                                                 gc.TypingArea.Width, 16);
+
+            using (Font font = new Font("Courier New", 10))
+                gc.Graphics.DrawString(message, font, Brushes.Red, position, TextStringFormat);
         }
 
         protected StringFormat TextStringFormat
@@ -358,6 +368,13 @@ namespace Typist
             }
         }
         private StringFormat singleCharStringFormat;
+
+        private class GraphicsContext
+        {
+            public Graphics Graphics;
+            public RectangleF TypingArea;
+            public RectangleF FirstCharArea;
+        }
 
         #endregion
     }
